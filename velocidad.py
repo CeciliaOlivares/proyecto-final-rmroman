1a68315b6483bf2494861074c85e1d5b5c01a9ba
CeciliaOlivares <46934765+CeciliaOlivares@users.noreply.github.com>
2019-01-22 11:05:18 -0600

    Cecilia Olivares, Grupo 03

---
+++ velocidad.py
@@ -0,0 +1,9 @@
+# Autor: Cecilia Olivares
+# Calcula velocidad promedio, dado el tiempo y la distancia
+
+distancia = int(input("Teclear distancia de tu viaje en km enteros: "))
+tiempo = int(input("Teclear tiempo de tu viaje en hrs enteros: "))
+
+velocidad = distancia/tiempo
+
+print ("velocidad promedio: ", velocidad, "km/h")
